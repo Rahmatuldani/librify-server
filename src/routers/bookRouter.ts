15bd25418cced558d6d12a46f5c9b27045655372
Rahmatul Ramadhani <danirahmatul@gmail.com>
2023-12-02 20:46:26 +0700

    pages

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBook, getBooks, deleteBook, getPoster, updateBook, changePoster } from '../controllers/bookController';
+import { createBook, getBooks, deleteBook, getPoster, updateBook, changePoster, getPages } from '../controllers/bookController';
 import { validateBook, validateUpdateBook } from '../middlewares/validator';
 import authorization from '../middlewares/authorization';
 import { upload } from '../uploads';
@@ -7,6 +7,7 @@ import { upload } from '../uploads';
 const bookRouter = express.Router();
 
 bookRouter.get('/', getBooks);
+bookRouter.get('/pages', getPages);
 bookRouter.post('/', authorization, upload.single('poster'), validateBook, createBook);
 bookRouter.put('/:id', authorization, validateUpdateBook, updateBook);
 bookRouter.put('/:id/changePoster', authorization, upload.single('poster'), changePoster);
